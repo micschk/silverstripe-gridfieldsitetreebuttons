1d7512b20b70ed3e2a07ee2ea55e88be56b43112
micschk <mvanschaik@gmail.com>
2014-05-19 12:10:12 +0200

    cleaning & removing duplicate code

--- code/GridFieldAddNewSiteTreeItemButton.php
+++ code/GridFieldAddNewSiteTreeItemButton.php
@@ -1,24 +1,8 @@
 <?php
 /**
  * This component provides a button for opening the default add new form for SiteTree items (Pages).
- *
- * @package framework
- * @subpackage gridfield
  */
-class GridFieldAddNewSiteTreeItemButton implements GridField_HTMLProvider {
-
-	protected $targetFragment;
-
-	protected $buttonName;
-
-	public function setButtonName($name) {
-		$this->buttonName = $name;
-		return $this;
-	}
-
-	public function __construct($targetFragment = 'before') {
-		$this->targetFragment = $targetFragment;
-	}
+class GridFieldAddNewSiteTreeItemButton extends GridFieldAddNewButton {
 
 	public function getHTMLFragments($gridField) {
 		if(!$this->buttonName) {
@@ -26,9 +10,9 @@ class GridFieldAddNewSiteTreeItemButton implements GridField_HTMLProvider {
 			$objectName = singleton($gridField->getModelClass())->i18n_singular_name();
 			$this->buttonName = _t('GridField.Add', 'Add {name}', array('name' => $objectName));
 		}
-		//http://localhost/askoschoenberg.nl/site/admin/pages/add/AddForm/?action_doAdd=1&ParentID=30&PageType=Page&SecurityID=b5de13b28bb75a91fec3901cf6ca4f48788e01da
-		//http://localhost/askoschoenberg.nl/site/admin/pages/add/AddForm/?action_doAdd=1&ParentID=16&PageType=NewsArticle&SecurityID=b5de13b28bb75a91fec3901cf6ca4f48788e01da
+		
 		$ParentID = Controller::curr()->getRequest()->param('ID');
+		// $ParentID = Controller::curr()->CurrentPageID();
 		$data = new ArrayData(array(
 			//'NewLink' => Controller::join_links($gridField->Link('item'), 'new'),
 			'NewLink' => Controller::join_links(Director::baseURL(),'/admin/pages/add/AddForm/', 
--- code/GridFieldEditSiteTreeItemButton.php
+++ code/GridFieldEditSiteTreeItemButton.php
@@ -1,65 +1,7 @@
 <?php
 
+class GridFieldEditSiteTreeItemButton extends GridFieldEditButton {
 
-class GridFieldEditSiteTreeItemButton implements GridField_ColumnProvider
-{
-	
-	/**
-	 * Add a column 'Delete'
-	 * 
-	 * @param type $gridField
-	 * @param array $columns 
-	 */
-	public function augmentColumns($gridField, &$columns) {
-		if(!in_array('Actions', $columns))
-			$columns[] = 'Actions';
-	}
-	
-	/**
-	 * Return any special attributes that will be used for FormField::createTag()
-	 *
-	 * @param GridField $gridField
-	 * @param DataObject $record
-	 * @param string $columnName
-	 * @return array
-	 */
-	public function getColumnAttributes($gridField, $record, $columnName) {
-		return array('class' => 'col-buttons');
-	}
-	
-	/**
-	 * Add the title 
-	 * 
-	 * @param GridField $gridField
-	 * @param string $columnName
-	 * @return array
-	 */
-	public function getColumnMetadata($gridField, $columnName) {
-		if($columnName == 'Actions') {
-			return array('title' => '');
-		}
-	}
-	
-	/**
-	 * Which columns are handled by this component
-	 * 
-	 * @param type $gridField
-	 * @return type 
-	 */
-	public function getColumnsHandled($gridField) {
-		return array('Actions');
-	}
-	
-	/**
-	 * Which GridField actions are this component handling
-	 *
-	 * @param GridField $gridField
-	 * @return array 
-	 */
-	public function getActions($gridField) {
-		return array();
-	}
-	
 	/**
 	 *
 	 * @param GridField $gridField
@@ -67,29 +9,16 @@ class GridFieldEditSiteTreeItemButton implements GridField_ColumnProvider
 	 * @param string $columnName
 	 * @return string - the HTML for the column 
 	 */
-	
 	public function getColumnContent($gridField, $record, $columnName) {
-		if(!$record->canEdit()){
+		if (!$record->canEdit()) {
 			return;
 		}
 		$data = new ArrayData(array(
 			//'Link' => Controller::join_links($gridField->Link('item'), $record->ID, 'edit')
-			'Link' => Controller::join_links(Director::baseURL(),'/admin/pages/edit/show/',$record->ID)
+			//'Link' => "admin/pages/edit/show/$record->ID"
+			'Link' => Controller::join_links(Director::baseURL(), '/admin/pages/edit/show/', $record->ID)
 		));
 		return $data->renderWith('GridFieldEditButton');
 	}
-	
-	/**
-	 * Handle the actions and apply any changes to the GridField
-	 *
-	 * @param GridField $gridField
-	 * @param string $actionName
-	 * @param mixed $arguments
-	 * @param array $data - form data
-	 * @return void
-	 */
-	public function handleAction(GridField $gridField, $actionName, $arguments, $data) {
-		
-	}	
-	
+
 }
